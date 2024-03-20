fcbb30daf34d357170c6c78ac03b71eca69f7c98
mmk <mmkkarbalaei@gmail.com>
2024-03-20 13:51:07 +0330

    operator style

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import './Calculator.css'
+import React from "react";
+import "./Calculator.css";
 
 const Calculator = () => {
   return (
-    <div className='container'>
-        <div className='screen'></div>
-        <div className='buttons'>
-            <button>Clear</button>
-            <button>C</button>
-            <button>÷</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>×</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>-</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>+</button>
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
-        </div>
+    <div className="container">
+      <div className="screen"></div>
+      <div className="buttons">
+        <button className="color twocol">Clear</button>
+        <button className="color">C</button>
+        <button className="color">÷</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="color">×</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button className="color">-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button className="color">+</button>
+        <button>0</button>
+        <button>.</button>
+        <button className="color twocol">=</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Calculator
+export default Calculator;
