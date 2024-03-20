e9b2857ba7f7337cabfa119f1012ca23aed63a7b
mmk <mmkkarbalaei@gmail.com>
2024-03-20 13:58:40 +0330

    state result

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Calculator.css";
 
 const Calculator = () => {
+  let [result , setResult ] = useState("");
+
+  const clickHandler = (e) => {
+     setResult(e.target.innerText);
+  };
   return (
     <div className="container">
-      <div className="screen"></div>
+      <div className="screen">{result}</div>
       <div className="buttons">
         <button className="color twocol">Clear</button>
         <button className="color">C</button>
-        <button className="color">÷</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button className="color">×</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button className="color">-</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button className="color">+</button>
-        <button>0</button>
-        <button>.</button>
+        <button onClick={clickHandler} className="color">÷</button>
+        <button onClick={clickHandler}>7</button>
+        <button onClick={clickHandler}>8</button>
+        <button onClick={clickHandler}>9</button>
+        <button onClick={clickHandler} className="color">×</button>
+        <button onClick={clickHandler}>4</button>
+        <button onClick={clickHandler}>5</button>
+        <button onClick={clickHandler}>6</button>
+        <button onClick={clickHandler} className="color">-</button>
+        <button onClick={clickHandler}>1</button>
+        <button onClick={clickHandler}>2</button>
+        <button onClick={clickHandler}>3</button>
+        <button onClick={clickHandler} className="color">+</button>
+        <button onClick={clickHandler}>0</button>
+        <button onClick={clickHandler}>.</button>
         <button className="color twocol">=</button>
       </div>
     </div>
