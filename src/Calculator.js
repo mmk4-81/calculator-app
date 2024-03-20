1573ae48c7c17ae4f022f18546f53cfae261dc9c
mmk <mmkkarbalaei@gmail.com>
2024-03-20 13:39:14 +0330

    component calculator

---
+++ src/Calculator.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Calculator.css'
+
+const Calculator = () => {
+  return (
+    <div className='container'>
+        <div className='screen'></div>
+        <div className='buttons'>
+            <button>Clear</button>
+            <button>C</button>
+            <button>÷</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>×</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>-</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+        </div>
+    </div>
+  )
+}
+
+export default Calculator
